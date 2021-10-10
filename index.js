091166ba4a108ba3c665767d716dc1d60eae0213
Kiran Hart <kiran.hart@live.com>
2021-10-10 00:15:52 -0400

    category fix

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ const fetchHeadInfo = async(headUrl) => {
 
     const id = Number(headUrl.split('/')[3].split('-')[0]);
     const name = $('#main > div > div.ym-col3 > div.ym-cbox.ym-clearfix > div > h2').text().trim()
-    const category = $('#main > div > div.ym-col3 > div.ym-cbox.ym-clearfix > div > a:nth-child(9)').text().trim();
+    
+    let category = null;
+
+    const categoryTry1 = $('#main > div > div.ym-col3 > div.ym-cbox.ym-clearfix > div > a:nth-child(10)').text().trim();
+    const categoryTry2 = $('#main > div > div.ym-col3 > div.ym-cbox.ym-clearfix > div > a:nth-child(9)').text().trim();
+
+    const possibleCategories = ['Alphabet', 'Animals', 'Blocks', 'Decoration', 'Food & Drinks', 'Humans', 'Humanoid', 'Miscellaneous', 'Monsters', 'Plants'];
+
+    if (possibleCategories.includes(categoryTry1)) {
+        category = categoryTry1;
+    } else if (possibleCategories.includes(categoryTry2)) {
+        category = categoryTry2;
+    }
+
     const tags = [];
     $('.ym-contain-fl a').toArray().map(e => {
         if ($(e).attr('href') != undefined && $(e).attr('href').startsWith('/custom-heads/tags/var/')) {
@@ -43,13 +56,20 @@ const fetchHeads = async(start) => {
 }
 
 (async() => {
-    const headUrlRequest = await Promise.all([fetchHeads(0), fetchHeads(80)]);
-    const headUrls = [].concat(...headUrlRequest);
+    const headUrlRequest = await Promise.all([
+        fetchHeads(0), 
+        fetchHeads(80)
+    ]);
 
+    const headUrls = [].concat(...headUrlRequest);
     const headInfo = await Promise.all(headUrls.map(fetchHeadInfo));
 
     const postHeads = await axios.post('https://rose.tweetzy.ca/minecraft/skulls', {
-        data: headInfo
+        data: headInfo,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
     })
 
     console.log(postHeads.data);
